eaf15343c6d518027bc88f2785fb9f9e0c2fdd31
nahid <abdolbaghinahid27@gmail.com>
2024-06-15 22:09:21 +0330

    force commit

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './Login.module.css'
+import './Login.css'
 const Login = () => { 
     const [email, setEmail] = useState(''); 
     const [password, setPassword] = useState(''); 
